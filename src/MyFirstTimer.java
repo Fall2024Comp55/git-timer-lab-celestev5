8bf30d241f2b401435f9229f4762c61250415feb
celestev5 <c_villa5@u.pacific.edu>
2024-09-30 22:32:01 -0700

    Three things a timer needs. A delay (time interval between each action), ActionListener (an object listening for an action to occur), and actionPerformed (called when an action occurs).

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,17 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private Timer someTimerVar;
+	private int numTimes;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -15,6 +21,28 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		
+		numTimes = 0;
+		
+		Timer someTimerVar = new Timer(1000, this);
+		
+		someTimerVar.setInitialDelay(3000);
+		
+		someTimerVar.start();
+		
+	}
+	
+	@Override 
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		
+		myLabel.setLabel("times called? " + numTimes);
+		
+		myLabel.move(5, 0);
+		
+		if(numTimes >= 10)
+			someTimerVar.stop();
+		
 	}
 	
 	public static void main(String[] args) {
